f5335ee6c5e36670464f836bba6b55cded4b2f15
Documenter.jl <documenter@juliadocs.github.io>
2022-03-14 14:11:20 +0000

    build based on 5e1e3ab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Math","category":"page"},{"location":"#Math","page":"Home","title":"Math","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Math.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Math]","category":"page"}]
+}
